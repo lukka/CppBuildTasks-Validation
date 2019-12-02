4c8e47beebff7bd85e6d6ad32c6625429a7f4dad
luca <681992+lukka@users.noreply.github.com>
2019-12-02 17:10:08 +0100

    tests for run-vcpkg and run-cmake GitHub actions

--- cmakesettings.json/test.cpp
+++ cmakesettings.json/test.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2019-2020 Luca Cappa
+// Released under the term specified in file LICENSE.txt
+// SPDX short identifier: MIT
+
 // main.cpp
 #include <sqlite3.h>
 #include <stdio.h>
